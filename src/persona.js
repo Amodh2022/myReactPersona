604f91115c771df1653d969581c0a0161f43b070
Amodh2022 <103436151+Amodh2022@users.noreply.github.com>
2023-12-05 22:52:30 +0530

    Add files via upload

--- src/persona.js
+++ src/persona.js
@@ -1,24 +1,23 @@
+// PersonaComponent.js
 import React, { useEffect } from 'react';
 import Persona from 'persona';
-import { useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, useParams } from 'react-router-dom';
+
 
 const PersonaComponent = () => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const userId = queryParams.get('userId');
-
+  const { userId } = useParams();
   useEffect(() => {
     const client = new Persona.Client({
       templateId: "itmpl_oFwr5vDFxPnJVnpKmXpgxY5x",
       environment: "sandbox",
       onReady: () => client.open(),
       onComplete: ({ inquiryId, status, fields }) => {
-        console.log(userId)
+        console.log(userId);
+        
+        if(status=="completed"){
 
-        if (status === "completed") {
-          // Do something when the Persona process is completed
-          window.location.href = 'https://www.google.com';
         }
+        window.location.href = 'https://www.google.com';
       },
       onCancel: ({ inquiryId, sessionToken }) => console.log('onCancel'),
       onError: (error) => console.log(error),
@@ -28,7 +27,7 @@ const PersonaComponent = () => {
     return () => {
       // Clean up if needed
     };
-  }, [userId]); // Make sure to include userId in the dependencies array
+  }, []); 
 
   return (
     <div>
