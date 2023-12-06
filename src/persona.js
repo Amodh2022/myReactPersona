3c0e1334013cca1f9ab0ff890025f009aaf9eb63
Amodh2022 <103436151+Amodh2022@users.noreply.github.com>
2023-12-06 12:26:53 +0530

    Add files via upload

--- src/persona.js
+++ src/persona.js
@@ -11,7 +11,7 @@ const PersonaComponent = () => {
   
     const urlParams=new URLSearchParams(window.location.search);
     const param1=urlParams.get('userId');
-    const decryptedUserId = decryptUserId(encryptedUserId);
+    const decryptedUserId = decryptUserId(encryptedUserId,"mySecret");
     setDecryptedUserId(decryptedUserId);
     console.log(param1);
     const client = new Persona.Client({
