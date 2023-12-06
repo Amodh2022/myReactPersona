1de8634ad7e62a32393c7daac6645d2d7d6e6350
Amodh2022 <103436151+Amodh2022@users.noreply.github.com>
2023-12-06 13:10:00 +0530

    Add files via upload

--- src/decrypt.js
+++ src/decrypt.js
@@ -1,22 +1,12 @@
-// decryptionModule.js
+import CryptoJS from 'crypto-js';
 
-export async function decryptUserId(encryptedUserId, secretKey) {
-    console.log(encryptedUserId);
+export function decryptUserId(encryptedUserId, secretKey) {
+  const key = CryptoJS.enc.Utf8.parse(secretKey);
+  const bytes = CryptoJS.AES.decrypt(encryptedUserId, key, {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7,
+  });
 
-    const key = await crypto.subtle.importKey(
-        'raw',
-        new TextEncoder().encode(secretKey),
-        { name: 'HMAC', hash: 'SHA-256' },
-        false,
-        ['sign']  // Add 'sign' to the list of usages
-    );
-
-    const data = new TextEncoder().encode(encryptedUserId);
-    const signature = await crypto.subtle.sign('HMAC', key, data);
-
-    const signatureHex = Array.from(new Uint8Array(signature))
-        .map((byte) => byte.toString(16).padStart(2, '0'))
-        .join('');
-
-    return signatureHex;
+  const decryptedUserId = bytes.toString(CryptoJS.enc.Utf8);
+  return decryptedUserId;
 }
