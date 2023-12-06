f6829291473fe71ce028c1dc510c6be88eca2a8a
Amodh2022 <103436151+Amodh2022@users.noreply.github.com>
2023-12-06 10:38:20 +0530

    Add files via upload

--- src/personaupdateapi.js
+++ src/personaupdateapi.js
@@ -1,12 +1,15 @@
-
-const apiUrl = 'https://bundeeapi1.azurewebsites.net/';
+const apiUrl = "https://bundeeapi1.azurewebsites.net/";
 
 export const callApi = async (personaEnquiryId, userId) => {
+  console.log(personaEnquiryId);
+  console.log(userId);
   try {
     const response = await fetch(`${apiUrl}/api/v1/user/createDriverProfile`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
+        bundee_auth_token:
+          "51699e35f393ba8a7642c80a245e5b8aad4200c961fa8469bae0ef984cb95e95d9f3677ca9a32e1838cfbd897297ab2a",
       },
       body: JSON.stringify({
         personaEnquiryId,
@@ -15,15 +18,12 @@ export const callApi = async (personaEnquiryId, userId) => {
     });
 
     if (!response.ok) {
-      throw new Error('API request failed');
+      throw new Error("API request failed");
     }
 
     const data = await response.json();
-    console.log('API response:', data);
-
-
   } catch (error) {
-    console.error('API error:', error);
-    throw Error("Error In Catch Block")
+    console.error("API error:", error);
+    throw Error("Error In Catch Block");
   }
 };
