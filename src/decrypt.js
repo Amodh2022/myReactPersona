576bf4285988041d1031375c4ebcb8b0e25acbab
Amodh2022 <103436151+Amodh2022@users.noreply.github.com>
2023-12-06 12:52:47 +0530

    Add files via upload

--- src/decrypt.js
+++ src/decrypt.js
@@ -1,23 +1,21 @@
-async function decryptUserId(encryptedUserId, secretKey) {
+
+
+export async function decryptUserId(encryptedUserId, secretKey) {
     console.log(encryptedUserId);
     const key = await crypto.subtle.importKey(
-      'raw',
-      new TextEncoder().encode(secretKey),
-      { name: 'HMAC', hash: 'SHA-256' },
-      false,
-      ['verify']
+        'raw',
+        new TextEncoder().encode(secretKey),
+        { name: 'HMAC', hash: 'SHA-256' },
+        false,
+        ['verify']
     );
-  
+
     const data = new TextEncoder().encode(encryptedUserId);
     const signature = await crypto.subtle.sign('HMAC', key, data);
-  
- 
-    const signatureHex = Array.from(new Uint8Array(signature))
-      .map((byte) => byte.toString(16).padStart(2, '0'))
-      .join('');
-  
-    return signatureHex;
-  }
-  
 
-  
+    const signatureHex = Array.from(new Uint8Array(signature))
+        .map((byte) => byte.toString(16).padStart(2, '0'))
+        .join('');
+
+    return signatureHex;
+}
