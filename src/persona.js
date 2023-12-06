fdf43e13feff0fbd3d4d578b0fe926f6d11f77f0
Amodh2022 <103436151+Amodh2022@users.noreply.github.com>
2023-12-06 16:23:37 +0530

    Add files via upload

--- src/persona.js
+++ src/persona.js
@@ -4,38 +4,32 @@ import Persona from 'persona';
 import { callApi } from './personaupdateapi'; 
 import { decrypt } from './decrypt'; // Replace with the actual path
 import CryptoJS from 'crypto-js';
+
 const PersonaComponent = () => {
-
-
   useEffect(() => {
-  
-    const urlParams=new URLSearchParams(window.location.search);
-    const encryptedUserId=urlParams.get('userId');
- 
-    // const decryptedUserId = atob(encryptedUserId);
-  
-    console.log("this")
-  
+    const urlParams = new URLSearchParams(window.location.search);
+    const encryptedUserId = urlParams.get('userId');
+
     const client = new Persona.Client({
       templateId: "itmpl_oFwr5vDFxPnJVnpKmXpgxY5x",
       environment: "sandbox",
       onReady: () => client.open(),
       onComplete: ({ inquiryId, status, fields }) => {
-     
-        if(status=="completed"){
-           callApi(inquiryId, encryptedUserId);
+        if (status === "completed") {
+          callApi(inquiryId, encryptedUserId);
+
+         
+          window.location.href = 'http://localhost:63945/#/request_to_book';
         }
-        
       },
       onCancel: ({ inquiryId, sessionToken }) => console.log('onCancel'),
       onError: (error) => console.log(error),
     });
-    client.open();
 
     return () => {
-      
+      // Cleanup code if needed
     };
-  }, []); 
+  }, []);
 
   return (
     <div>
